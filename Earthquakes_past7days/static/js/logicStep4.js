e43df264fb0eb570f1713732896ed9ff70a0a9fd
Ashok Ramaswami <aramaswami@gmail.com>
2020-06-11 19:35:03 -0700

    updates

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -247,3 +247,16 @@ function getColor(magnitude) {
     }
     return "#98ee00";
   }
+
+  // Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+    Earthquakes: earthquakes
+  };
+
+// Then we add a control to the map that will allow the user to change
+// which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
